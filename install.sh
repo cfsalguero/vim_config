e372f9028d011325d3f3887facfcc3081997d641
Carlos Salguero <cfsalguero@gmail.com>
2016-01-08 02:29:00 -0300

    Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https@github.com:Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe/
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe/
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/scrooloose/nerdtree.git ~/vim/bundle/nerdtree
 
