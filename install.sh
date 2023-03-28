8ff5b3b350bb5d7d4fc64853f026cb256c658ead
Carlos Salguero <carlos.salguero@percona.com>
2023-03-28 14:46:27 -0300

    Changed plugin manager

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 DATE=$(date +%FT%T%z)
 VIMRC="${HOME}/.vimrc"
 
-echo "need to install build-essential & cmake"
-sudo apt-get install -y build-essential cmake git vim neovim
-sudo apt-get install -y python3-dev python3-pip python-neovim python3-neovim
-sudo pip install --upgrade neovim
-
 if [ -a ${HOME}/.vimrc ];
 then
     echo "Found ${VIMRC}"
@@ -22,6 +17,11 @@ fi
 
 mkdir -p ${HOME}/.vim/undodir
 
+# Vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Neo Vim
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -s ${HOME}/.vim/.vimrc ${VIMRC} 
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -30,9 +30,14 @@ git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 git clone https://github.com/scrooloose/nerdtree.git ~/vim/bundle/nerdtree
 
 # YCM
+echo "need to install build-essential & cmake"
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y python3-dev python3-pip python-neovim python3-neovim
+sudo pip install --upgrade neovim
+
 cd ~/.vim/bundle/YouCompleteMe/
 git submodule update --init --recursive
-./install.py --gocode-completer
+python3 install.py --all
 
 cd ~/
 vim +PluginInstall +qall
@@ -40,6 +45,7 @@ vim +PluginInstall +qall
 # Fix for NerdTree. If we try to open but the file is already opened, 
 # just jump to the that instead of opening it twice.
 cp override_tab_mapping.vim bundle/nerdtree/nerdtree_plugin/
+
 
 # PowerFonts for PowerLine
 cd /tmp
@@ -53,5 +59,5 @@ sudo fc-cache -vf
 sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 mkdir -p ${HOME}/.config/nvim/
-rm -f /home/karl/.config/nvim/init.vim
+rm -f ~/.config/nvim/init.vim
 ln -s ~/.vim/.vimrc ~/.config/nvim/init.vim
